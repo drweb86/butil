8fc1a2cc617823935ebafad11f377163d62c0b7a
Siarhei Kuchuk <cuchuk.sergey@gmail.com>
2023-09-20 22:16:46 +0300

    publishing as 7z archive

--- sources/publish.cmd
+++ sources/publish.cmd
@@ -11,6 +11,11 @@ if NOT %ERRORLEVEL% == 0 (
 	GOTO error;
 )
 
+del ..\Output\butil-binaries.7z
+"c:\Program Files\7-Zip\7z.exe" a -y ..\Output\butil-binaries.7z "..\Output\BUtil\*" -mx9 -t7z -m0=lzma2 -ms=on -sccUTF-8 -ssw
+if NOT %ERRORLEVEL% == 0 (
+	GOTO error;
+)
 
 echo NOW SEND FILE TO https://www.microsoft.com/en-us/wdsi/filesubmission to fix smart screen detection
 exit 0
