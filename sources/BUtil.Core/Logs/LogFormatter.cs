09ccf9c325331f7544f26d301ad35f5faabeba34
Siarhei Kuchuk <cuchuk.sergey@gmail.com>
2023-11-24 20:05:46 +0300

    ubuntu snap

---
+++ sources/BUtil.Core/Logs/LogFormatter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace BUtil.Core.Logs
+{
+    internal static class LogFormatter
+    {
+        public static string GetFormattedMessage(LoggingEvent loggingEvent, string message)
+        {
+            var information = string.Format(CultureInfo.CurrentUICulture,
+                            "{0:HH:mm} {1}",
+                            DateTime.Now,
+                            message);
+
+            if (loggingEvent == LoggingEvent.Error)
+                return $"⛔ERROR {information}";
+            else
+                return $"✅ {information}";
+        }
+    }
+}
