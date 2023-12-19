5588d8158302a148c489384e42910c5c72213f7a
Siarhei Kuchuk <cuchuk.sergey@gmail.com>
2023-12-19 23:17:03 +0300

    delete version tests - delete recent version

---
+++ sources/BUtil.Core/TasksTree/IncrementalModel/DeleteVersionUtil.cs
@@ -0,0 +1,68 @@
+﻿using BUtil.Core.ConfigurationFileModels.V2;
+using BUtil.Core.State;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUtil.Core.TasksTree.IncrementalModel
+{
+    internal class DeleteVersionUtil
+    {
+        public static void DeleteVersion(IncrementalBackupState state, VersionState versionToDelete, 
+            out IEnumerable<string> storageFilesToDelete)
+        {
+            var storageFilesToDeleteList = new List<string>();
+            storageFilesToDelete = storageFilesToDeleteList;
+
+
+            var orderedVersions = state.VersionStates
+                .OrderByDescending(x => x.BackupDateUtc)
+                .ToList();
+            var lastVersion = orderedVersions.First();
+
+            // if last version is deleted
+            if (lastVersion == versionToDelete)
+            {
+                state.VersionStates.Remove(versionToDelete);
+                versionToDelete.SourceItemChanges
+                    .ToList()
+                    .ForEach(sourceItemChange =>
+                    {
+                        storageFilesToDeleteList.AddRange(sourceItemChange.UpdatedFiles.Select(x => x.StorageFileName).ToList());
+                        storageFilesToDeleteList.AddRange(sourceItemChange.CreatedFiles.Select(x => x.StorageFileName).ToList());
+                    });
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+
+            RemoveUnchangedFiles(state, storageFilesToDeleteList);
+        }
+
+        private static void RemoveUnchangedFiles(IncrementalBackupState state, List<string> storageFileNamesToDelete)
+        {
+            foreach (var version in state.VersionStates)
+            {
+                foreach (var sourceItemChange in version.SourceItemChanges)
+                {
+                    foreach(var file in  sourceItemChange.UpdatedFiles)
+                    {
+                        var storageFileName = file.StorageFileName;
+                        if (storageFileNamesToDelete.Contains(storageFileName))
+                            storageFileNamesToDelete.Remove(storageFileName);
+                    }
+
+                    foreach (var file in sourceItemChange.CreatedFiles)
+                    {
+                        var storageFileName = file.StorageFileName;
+                        if (storageFileNamesToDelete.Contains(storageFileName))
+                            storageFileNamesToDelete.Remove(storageFileName);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ sources/BUtil.Core/_Tests/DeleteVersionTests.cs
@@ -0,0 +1,73 @@
+﻿
+using BUtil.Core.State;
+using BUtil.Core.TasksTree.IncrementalModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace BUtil.Core._Tests
+{
+    [TestClass]
+    public class DeleteVersionTests
+    {
+        private StorageFile StorageFileEx(int fileId, int storageFileId)
+        {
+            return new StorageFile(new FileState(fileId.ToString(), DateTime.MinValue, 0, string.Empty)) { StorageFileName = storageFileId.ToString() };
+        }
+
+        [TestMethod]
+        public void DeleteLastVersion()
+        {
+            // set
+            var sourceItem1 = new ConfigurationFileModels.V2.SourceItemV2(@"x:\test", true);
+
+            var version1 = new VersionState(new System.DateTime(2000, 1, 1), new[] {
+                new SourceItemChanges(
+                    sourceItem1,
+                    // deleted
+                    new System.Collections.Generic.List<string>() { @"x:\test\deleted" },
+                    // updated
+                    new System.Collections.Generic.List<StorageFile> { StorageFileEx(1, 1) },
+                    // created
+                    new System.Collections.Generic.List<StorageFile> { StorageFileEx(2, 2) }
+                ) } );
+
+            var version2 = new VersionState(new System.DateTime(3000, 1, 1), new[] {
+                new SourceItemChanges(
+                    sourceItem1,
+                    // deleted
+                    new System.Collections.Generic.List<string>() { @"x:\test\updated1" },
+                    // updated
+                    new System.Collections.Generic.List<StorageFile> { StorageFileEx(2, 2) },
+                    // created
+                    new System.Collections.Generic.List<StorageFile> { StorageFileEx(2, 3) }
+                ) });
+            var version3 = new VersionState(new System.DateTime(4000, 1, 1), new[] {
+                new SourceItemChanges(
+                    sourceItem1,
+                    // deleted
+                    new System.Collections.Generic.List<string>() { @"x:\test\updated1" },
+                    // updated
+                    new System.Collections.Generic.List<StorageFile> { StorageFileEx(1, 3) },
+                    // created
+                    new System.Collections.Generic.List<StorageFile> { StorageFileEx(2, 4) }
+
+                ) });
+
+            var versions = new IncrementalBackupState();
+            versions.VersionStates.Add(version1);
+            versions.VersionStates.Add(version2);
+            versions.VersionStates.Add(version3);
+
+            // act
+            DeleteVersionUtil.DeleteVersion(versions, version3, out var storageFilesToDelete);
+
+            // assert
+            Assert.AreEqual(versions.VersionStates.Count, 2);
+
+            var filesToDelete = storageFilesToDelete.ToArray();
+            Assert.AreEqual(filesToDelete.Length, 1);
+            Assert.AreEqual(filesToDelete[0], 4.ToString());
+        }
+    }
+}
