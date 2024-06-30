f71f26a6c75b3a0199290ebe1cfee4e8d6e5a081
drweb86 <4712173+drweb86@users.noreply.github.com>
2024-06-30 20:47:19 +0300

    test shortcuts

--- sources/build-ubuntu.sh
+++ sources/build-ubuntu.sh
@@ -25,20 +25,45 @@ cd ./sources
 dotnet publish /p:Version=${version} /p:AssemblyVersion=${version} -c Release --self-contained true -o ../.${outputFolder}
 
 cd ../..
+echo Creating shortcuts
+
+cp ${tempFolder}sources/butil-ui/Assets/butil.ico ${outputFolder}/butil.ico
+
+uiShortcut=${HOME}/Desktop/BUtil.desktop
+
+cat > ${uiShortcut} << EOL
+[Desktop Entry]
+Encoding=UTF-8
+Version=1.0
+Type=Application
+Terminal=false
+Exec=$(pwd)/${outputFolder}/butil-ui.Desktop
+Name=BUtil
+Icon=$(pwd)/${outputFolder}/butil.ico
+EOL
+
+chmod -R 775 ${uiShortcut}
+gio set "${uiShortcut}" metadata::trusted yes
+
+consoleShortcut="${HOME}/Desktop/BUtil Console.desktop"
+
+cat > "${consoleShortcut}" << EOL
+[Desktop Entry]
+Encoding=UTF-8
+Version=1.0
+Type=Application
+Terminal=true
+Exec=$(pwd)/${outputFolder}/butilc
+Name=BUtil Console
+Icon=$(pwd)/${outputFolder}/butil.ico
+EOL
+
+chmod -R 775 "${consoleShortcut}"
+gio set "${consoleShortcut}" metadata::trusted yes
+
 rm -rf ${tempFolder}
 
 echo Everything is completed 
 echo Folder      : ${outputFolder}
-echo UI tool     : butil-ui.Desktop
-echo Console tool: butilc
-
-# "~/Desktop/BUtil ${version}.desktop" << EOF 
-# [Desktop Entry]
-# Encoding=UTF-8
-# Version=1.0
-# Type=Application
-# Terminal=false
-# Exec=$1
-# Name=$2
-# Icon=$3
-# EOF
+echo UI tool     : butil-ui.Desktop, see desktop shortcut
+echo Console tool: butilc, see desktop shortcut
