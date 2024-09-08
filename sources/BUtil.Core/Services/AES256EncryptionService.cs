4a1e8bbbf77c7abb564a6e83ed4630dea9d7acdc
drweb86 <4712173+drweb86@users.noreply.github.com>
2024-09-08 12:16:41 +0300

    aes in progress

---
+++ sources/BUtil.Core/Services/AES256EncryptionService.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BUtil.Core.Services;
+
+internal static class AES256EncryptionService
+{
+    static void EncryptFile(string inputFile, string outputFile, string password, string iv)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.KeySize = 256;
+            aes.BlockSize = 128;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            byte[] key = DeriveKeyFromPassword(password, aes.KeySize);
+            aes.Key = key;
+            aes.IV = Encoding.UTF8.GetBytes(iv.PadRight(16, '0').Substring(0, 16));
+
+            using (FileStream inputFileStream = new FileStream(inputFile, FileMode.Open))
+            using (FileStream outputFileStream = new FileStream(outputFile, FileMode.Create))
+            using (ICryptoTransform encryptor = aes.CreateEncryptor())
+            using (CryptoStream cryptoStream = new CryptoStream(outputFileStream, encryptor, CryptoStreamMode.Write))
+            {
+                inputFileStream.CopyTo(cryptoStream);
+            }
+        }
+    }
+
+    static void DecryptFile(string inputFile, string outputFile, string password, string iv)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.KeySize = 256;
+            aes.BlockSize = 128;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            byte[] key = DeriveKeyFromPassword(password, aes.KeySize);
+            aes.Key = key;
+            aes.IV = Encoding.UTF8.GetBytes(iv.PadRight(16, '0').Substring(0, 16));
+
+            using (FileStream inputFileStream = new FileStream(inputFile, FileMode.Open))
+            using (FileStream outputFileStream = new FileStream(outputFile, FileMode.Create))
+            using (ICryptoTransform decryptor = aes.CreateDecryptor())
+            using (CryptoStream cryptoStream = new CryptoStream(inputFileStream, decryptor, CryptoStreamMode.Read))
+            {
+                cryptoStream.CopyTo(outputFileStream);
+            }
+        }
+    }
+
+    static byte[] DeriveKeyFromPassword(string password, int keySizeInBits)
+    {
+        const int iterations = 10000;
+        using (var deriveBytes = new Rfc2898DeriveBytes(password, Encoding.UTF8.GetBytes("saltysalt"), iterations, HashAlgorithmName.SHA256))
+        {
+            return deriveBytes.GetBytes(keySizeInBits / 8);
+        }
+    }
+}
--- sources/BUtil.Core/State/StorageMethodNames.cs
+++ sources/BUtil.Core/State/StorageMethodNames.cs
@@ -3,4 +3,5 @@
 public static class StorageMethodNames
 {
     public const string SevenZipEncrypted = "Encrypted 7z";
+    public const string Aes256Encrypted = "Encrypted AES256";
 }
